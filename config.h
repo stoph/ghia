f238227900657833a1c169e53c7e94398f833967
stoph <chrtistoph.khouri@gmail.com>
2021-03-06 13:21:58 -0500

    Config

---
+++ config.h
@@ -0,0 +1,9 @@
+/*
+ * Configuration
+ */
+
+// Minimum RSSI signal strength to react to a known device
+#define minRSSI -90
+
+// MAC addresses from known device MACs
+String knownMACs[] = { "aa:bb:cc:dd:ee:ff", "11:22:33:44:55:66"};
